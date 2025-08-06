b8412ce05c4e1eb7d804ae205806d5767816e065
lina palacios <linam.palacios@gmail.com>
2025-08-06 01:29:31 +0000

    dag vault progress

---
+++ dbt/dbt_neobank/models/data_vault_test/hubs/hub_transactions_test.sql
@@ -0,0 +1,6 @@
+select
+    transaction_id,
+    md5(cast(transaction_id as string)) as transaction_hk
+    ,{{ load_date() }} as load_date
+    ,{{ record_source('stg_transactions_test') }} as record_source
+from {{ ref('stg_transactions_test') }}
---
+++ dbt/dbt_neobank/models/data_vault_test/links/link_user_transaction_test.sql
@@ -0,0 +1,18 @@
+with transactions_with_user as (
+    select
+        t.transaction_id,
+        u.user_id,
+        md5(cast(u.user_id as string)) as user_hk,
+        md5(cast(t.transaction_id as string)) as transaction_hk
+    from {{ ref('stg_transactions_test') }} as t
+    inner join {{ ref('stg_users_test') }} as u
+        on t.user_id = u.user_id
+)
+
+select
+    md5(concat(user_hk, transaction_hk)) as user_transaction_hk,
+    user_hk,
+    transaction_hk
+    ,{{ load_date() }} as load_date
+    ,{{ record_source('stg_transactions_test') }} as record_source
+from transactions_with_user
